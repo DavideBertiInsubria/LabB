0ad8c0f4c375d27c053831470011383179866152
RIP-blip <55853176+RIP-blip@users.noreply.github.com>
2021-06-10 13:00:31 +0200

    Scheletro del progetto

--- src/main/java/centrivaccinali/ServerImpl.java
+++ src/main/java/centrivaccinali/ServerImpl.java
@@ -5,10 +5,15 @@ import common.Cittadino;
 import common.TipologiaCentro;
 import common.Vaccinato;
 
+import java.io.IOException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class ServerImpl extends UnicastRemoteObject implements ServerInterface {
+
+    private Database DBconnetion;
 
     @Override
     public void registraCittadino (Cittadino c){
@@ -34,4 +39,22 @@ public class ServerImpl extends UnicastRemoteObject implements ServerInterface {
     public String visualizzaInfoCentroVaccinale (CentroVaccinale CV) {
         return null;
     }
+
+    private void exec(){
+        try {
+            ServerImpl serverR= new ServerImpl ();
+            Registry registro = LocateRegistry.createRegistry (1099);
+            registro.rebind ("Vaccino", serverR);
+
+        } catch (
+                IOException e) {
+            e.printStackTrace ();
+        }
+    }
+    public static void main(String args[])throws Exception {//Main per inizializzare il server
+
+        ServerImpl server = new ServerImpl ();
+        server.exec();
+    }
+
 }

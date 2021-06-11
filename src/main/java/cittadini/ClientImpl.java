807d2e207e53ab69b4e582f9130a8dae2bdc1f63
grotto <davide99.db@libero.it>
2021-06-11 13:33:22 +0200

    RMI Client 0.1

--- src/main/java/cittadini/ClientImpl.java
+++ src/main/java/cittadini/ClientImpl.java
@@ -5,6 +5,9 @@ import java.rmi.server.UnicastRemoteObject;
 
 public class ClientImpl extends UnicastRemoteObject implements ClientInterface {
 
+    protected ClientImpl() throws RemoteException {
+    }
+
     @Override
     public String msg(String messaggio) throws RemoteException {
         return messaggio;

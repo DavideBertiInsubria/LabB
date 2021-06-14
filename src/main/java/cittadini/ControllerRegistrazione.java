3160ce1eed75106f7bb34c8631f6ad04140e2e5b
Aleksandar Evgeniev Ivanov <ivanov.aleksandar.ev@gmail.com>
2021-06-14 14:30:12 +0200

    GUIcittadini 0.2

--- src/main/java/cittadini/ControllerRegistrazione.java
+++ src/main/java/cittadini/ControllerRegistrazione.java
@@ -7,15 +7,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 
 public class ControllerRegistrazione {
-    @FXML
-    Button btnProva;
 
     public void clickProva(ActionEvent event)  {
         try {

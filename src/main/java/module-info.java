66dcafc32327cf2a8b08a8f1d6b6165bceb58bad
s93r <84583922+s93r@users.noreply.github.com>
2021-06-15 18:49:02 +0200

    GUI centri vaccinali 1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,9 @@ module LabB {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.rmi;
+    opens centrivaccinali to javafx.fxml;
     opens cittadini to javafx.fxml;
+    exports centrivaccinali;
     exports cittadini;
     exports common;
 }

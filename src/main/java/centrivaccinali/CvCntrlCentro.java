54b8340ddbbf230193a5f495fa3dcfb278e456fd
s93r <84583922+s93r@users.noreply.github.com>
2021-06-29 11:30:47 +0200

    GUIcv : CvCentro w/ field controls update

--- src/main/java/centrivaccinali/CvCntrlCentro.java
+++ src/main/java/centrivaccinali/CvCntrlCentro.java
@@ -1,22 +1,30 @@
 package centrivaccinali;
 
+import common.CentroVaccinale;
+import common.ClientImpl;
+import common.ClientInterface;
 import common.TipologiaCentro;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import server.ServerInterface;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import javax.swing.*;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class CvCntrlCentro {
     @FXML
-    private TextField txtNome, txtIndirizzoNome, txtIndirizzoNumero, txtIndirizzoComune, txtIndirizzoProvincia, txtIndirizzoCap;
+    private TextField txtName, txtAddrName, txtAddrNumb, txtAddrCity, txtAddrProv, txtAddrZipc;
     @FXML
     private RadioButton btnVia, btnViale, btnPiazza, btnAziendale, btnHub, btnOspedaliero;
+
+
 
     public void backFromCentroVaccinale(ActionEvent event) {
         // CHIUSURA DELLA VECCHIA FINESTRA
@@ -25,43 +33,95 @@ public class CvCntrlCentro {
         // APERTURA DELLA NUOVA FINESTRA
         new CvHomePage();
     }
-    public void registraCentroVaccinale(ActionEvent event) {
-        // NOME
-        String name = txtNome.getText();
-        // INDIRIZZO
-        String addrTp = null;
-        if(btnVia.isSelected()) {
-            addrTp = btnVia.getText();
-        } else if(btnViale.isSelected()) {
-            addrTp = btnViale.getText();
-        } else if (btnPiazza.isSelected()) {
-            addrTp = btnPiazza.getText();
+    public void confirm(ActionEvent event) {
+        if(check()) {
+            // NOME del centro vaccinale
+            String name = txtName.getText().trim().replaceAll("\\s+", " ");
+            // INDIRIZZO del centro vaccinale
+            // CAMPO N.1: QUALIFICATORE
+            String field1 = null;
+            if(btnVia.isSelected()) {
+                field1 = btnVia.getText();
+            } else if(btnViale.isSelected()) {
+                field1 = btnViale.getText();
+            } else if (btnPiazza.isSelected()) {
+                field1 = btnPiazza.getText();
+            }
+            // CAMPO N.2: NOME
+            String field2 = txtAddrName.getText().trim().replaceAll("\\s+", " ");
+            // CAMPO N.3: NUMERO CIVICO
+            String field3 = txtAddrNumb.getText().trim().replaceAll("\\s+", " ");
+            // CAMPO N.4: COMUNE
+            String field4 = txtAddrCity.getText().trim().replaceAll("\\s+", " ");
+            // CAMPO N.5: PROVINCIA
+            String field5 = txtAddrProv.getText().trim().replaceAll("\\s+", " ");
+            // CAMPO N.6: CODICE DI AVVIAMENTO POSTALE
+            String field6 = txtAddrZipc.getText().trim().replaceAll("\\s+", " ");
+            // TIPOLOGIA del centro vaccinale
+            TipologiaCentro type = null;
+            if(btnAziendale.isSelected()) {
+                type = TipologiaCentro.AZIENDALE;
+            } else if(btnHub.isSelected()) {
+                type = TipologiaCentro.HUB;
+            } else if(btnOspedaliero.isSelected()) {
+                type = TipologiaCentro.OSPEDALIERO;
+            }
+            // COSTRUZIONE INDIRIZZO
+            String addr = field1 + " " + field2 + " " + field3 + ", " + field4 + " (" + field5 + "), " + field6;
+            // COSTRUZIONE CENTRO VACCINALE
+            CentroVaccinale cv = new CentroVaccinale(name, addr, type);
+            // COLLEGAMENTO A SERVER
+            try {
+                Registry reg = LocateRegistry.getRegistry("*", 1099);
+                ServerInterface server = (ServerInterface) reg.lookup("Vaccino");
+                ClientInterface client = new ClientImpl();
+                server.registraCentroVaccinale(cv, client);
+            } catch (RemoteException | NotBoundException e) {
+                JOptionPane.showMessageDialog(null, "Connessione al server fallita.", "Error", JOptionPane.ERROR_MESSAGE);
+                System.exit(0);
+            }
+            // CHIUSURA VECCHIA FINESTRA
+            Stage oldStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            oldStage.close();
+            // APERTURA NUOVA FINESTRA
+            new CvHomePage();
         }
-        String addrNm = txtIndirizzoNome.getText();
-        String addrNo = txtIndirizzoNumero.getText();
-        String addrCm = txtIndirizzoComune.getText();
-        String addrPr = txtIndirizzoProvincia.getText();
-        String addrZc = txtIndirizzoCap.getText();
-        StringBuilder sb = null;
-        sb.append(addrTp);
-        sb.append(addrNm);
-        sb.append(addrNo);
-        sb.append(addrCm);
-        sb.append(addrPr);
-        sb.append(addrZc);
-        String addr = sb.toString();
-        // TIPOLOGIA
-        TipologiaCentro type = null;
-        if(btnAziendale.isSelected()) {
-            type = TipologiaCentro.AZIENDALE;
-        } else if(btnHub.isSelected()) {
-            type = TipologiaCentro.HUB;
-        } else if(btnOspedaliero.isSelected()) {
-            type = TipologiaCentro.OSPEDALIERO;
-        }
-        // System.out.println(name + addr + type.toString());
-
-        /* COSTRUTTORE CENTRO VACCINALE: CentroVaccinale cv = new CentroVaccinale(String nome, String addr, TipologiaCentro type); */
-        /* INVIO DATI A DATABASE */
     }
+    private boolean check() {
+        if(txtName.getText().isBlank() || txtAddrName.getText().isBlank() || txtAddrNumb.getText().isBlank() || txtAddrCity.getText().isBlank() || txtAddrProv.getText().isBlank() || txtAddrZipc.getText().isBlank()) {
+            JOptionPane.showMessageDialog(null, "Tutti i campi devono essere compilati.", "Warning", JOptionPane.WARNING_MESSAGE);
+            return false;
+        }
+        if(txtAddrProv.getText().length() != 2) {
+            JOptionPane.showMessageDialog(null, "La sigla di una provincia contiene un numero massimo di due caratteri.", "Warning", JOptionPane.WARNING_MESSAGE);
+            return false;
+        }
+        return true;
+    }
+
+    /*
+    private static boolean isAlphabetical(String s) {
+        String[] arr = s.split(" ");
+        boolean alphabetical;
+        for (String value : arr) {
+            alphabetical = value.chars().allMatch(Character::isLetter);
+            if (!alphabetical) {
+                return false;
+            }
+        }
+        return true;
+    }
+    */
+
+    /*
+    private static boolean isNumerical(String s) {
+        try {
+            int i = Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+    */
+
 }
--- src/main/java/centrivaccinali/CvCntrlVaccinato.java
+++ src/main/java/centrivaccinali/CvCntrlVaccinato.java
@@ -2,19 +2,28 @@ package centrivaccinali;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class CvCntrlVaccinato {
+import javax.swing.*;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
+
+public class CvCntrlVaccinato implements Initializable {
     @FXML
-    private DatePicker selectDate;
+    private DatePicker dataSomministrazione;
     @FXML
     private RadioButton btnAstraZeneca, btnJohnsonJohnson, btnModerna, btnPfizer;
     @FXML
     private TextField txtCentroVaccinale, txtNomeVaccinato, txtCognomeVaccinato, txtCfVaccinato, txtIdVaccinato;
+
+
 
     public void backFromVaccinato(ActionEvent event) {
         // CHIUSURA DELLA VECCHIA FINESTRA
@@ -24,6 +33,46 @@ public class CvCntrlVaccinato {
         new CvHomePage();
     }
     public void registraVaccinato(ActionEvent event) {
-        /* */
+        if(controllaCampi()) {
+            String campo1 = txtCentroVaccinale.getText();
+            String campo2 = txtNomeVaccinato.getText();
+            String campo3 = txtCognomeVaccinato.getText();
+            String campo4 = txtCfVaccinato.getText();
+            LocalDate date = dataSomministrazione.getValue();
+            String myDate = date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+            String campo5 = null;
+            if(btnAstraZeneca.isSelected()) {
+                campo5 = btnAstraZeneca.getText();
+            } else if(btnJohnsonJohnson.isSelected()) {
+                campo5 = btnJohnsonJohnson.getText();
+            } else if(btnModerna.isSelected()) {
+                campo5 = btnModerna.getText();
+            } else if(btnPfizer.isSelected()) {
+                campo5 = btnPfizer.getText();
+            }
+            String campo6 = txtIdVaccinato.getText();
+
+            String vacc = campo1+campo2+campo3+campo4+date.toString()+campo5+campo6;
+            System.out.println(vacc);
+        }
+
+    }
+    private boolean controllaCampi() {
+        if(txtCentroVaccinale.getText().equals("")||txtNomeVaccinato.getText().equals("")||txtCognomeVaccinato.getText().equals("")||txtCfVaccinato.getText().equals("")||txtIdVaccinato.getText().equals("")) {
+            JOptionPane.showMessageDialog(null, "Compilare tutti i campi.");
+            return false;
+        }
+        return true;
+    }
+    public void getDate(ActionEvent e) {
+        LocalDate myDate = dataSomministrazione.getValue();
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        dataSomministrazione.setValue(LocalDate.parse(formatter.format(LocalDate.now())));
+        //dataSomministrazione.setValue(LocalDate.parse());
     }
 }

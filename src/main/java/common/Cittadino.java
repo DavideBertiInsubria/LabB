7f9e4ad34e9c930255cf57434c5fd549c934232b
grotto <davide99.db@libero.it>
2021-08-06 10:37:30 +0200

    Classi common 0.1 (JavaDOC)

--- src/main/java/common/Cittadino.java
+++ src/main/java/common/Cittadino.java
@@ -12,15 +12,15 @@ public class Cittadino extends Vaccinato implements Serializable {
     private int idvaccinazione;
 
     /**
-     *
-     * @param cf
-     * @param nome
-     * @param cognome
-     * @param userID
-     * @param email
-     * @param pwd
-     * @param idvaccinazione
-     * @param nomecentro
+     *Costruttore per creare un oggetto di tipo <i>Cittadino</i> che richiama i metodi della superclasse vaccinato per assegnargli i valori dei parametri.
+     * @param cf Parametro di tipo <i>String</i> per identificare il codice fiscale.
+     * @param nome Parametro di tipo <i>String</i> per identificare il nome.
+     * @param cognome Parametro di tipo <i>String</i> per identificare il cognome.
+     * @param userID Parametro di tipo <i>String</i> per identificare il nickname.
+     * @param email Parametro di tipo <i>String</i> per identificare la email.
+     * @param pwd Parametro di tipo <i>String</i> per identificare la password.
+     * @param idvaccinazione Parametro di tipo <i>Int</i> per identificare l'ID vaccinazione.
+     * @param nomecentro Parametro di tipo <i>String</i> per identificare il nome del centro.
      */
     public Cittadino(String cf,String nome,String cognome, String userID, String email,String pwd,int idvaccinazione,String nomecentro) {
        
@@ -36,27 +36,21 @@ public class Cittadino extends Vaccinato implements Serializable {
     }
 
     /**
-     *
-     * @return
+     *Il metodo <em>getEmail</em> serve a ottenere l'email del cittadino.
+     * @return Il valore dell'email.
      */
-    public String getEmail() {
-        return email;
-    }
+    public String getEmail() {return email;}
 
     /**
-     *
-     * @return
+     *Il metodo <em>getPassword</em> serve a ottenere la password del cittadino.
+     * @return Il valore della password.
      */
-    public String getPassword() {
-        return password;
-    }
+    public String getPassword() {return password;}
 
     /**
-     *
-     * @return
+     *Il metodo <em>getUserID</em> serve a ottenere lo UserID del cittadino.
+     * @return Il valore dello UserID.
      */
-    public String getUserID() {
-    	return userID;
-    }
+    public String getUserID() {return userID;}
     
 }

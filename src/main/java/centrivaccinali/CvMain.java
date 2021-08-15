3e981dc03878b9f642ba00aa870e1d2b0b75f3ae
s93r <84583922+s93r@users.noreply.github.com>
2021-08-15 18:08:00 +0200

    GUIcv : CvMain : Javadoc v.1

--- src/main/java/centrivaccinali/CvMain.java
+++ src/main/java/centrivaccinali/CvMain.java
@@ -3,12 +3,30 @@ package centrivaccinali;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * <p>
+ *     Il punto di ingresso dell'applicazione destinata all'uso da parte di centri vaccinali e cittadini vaccinati.
+ * </p>
+ * @author Berti Davide
+ * @author Ivanov Aleksandar Evgeniev
+ * @author Mazza Serghej
+ * @author Rizzi Silvio 719638 VA
+ */
 public class CvMain extends Application {
 
     public static void main(String[] args) {
         launch(args);
     }
 
+    /**
+     * <p>
+     *     Avvia l'applicazione aprendo una nuova finestra in cui &egrave visualizzata la pagina iniziale.
+     * </p>
+     * <p>
+     *     <em>NOTA: questo metodo sovrascrive il metodo astratto <code>start</code> della classe <code>Application</code>.</em>
+     * </p>
+     * @see Application
+     */
     @Override
     public void start(Stage primaryStage) {
         new CvHomePage();

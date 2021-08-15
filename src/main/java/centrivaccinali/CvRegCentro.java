945003f7e252f4ba43e7fd692afeaf08e3096105
s93r <84583922+s93r@users.noreply.github.com>
2021-08-15 18:59:59 +0200

    GUIcv : CvRegCentro : Javadoc v.1

--- src/main/java/centrivaccinali/CvRegCentro.java
+++ src/main/java/centrivaccinali/CvRegCentro.java
@@ -6,7 +6,25 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * <p>
+ *     L'interfaccia grafica che implementa il modulo per la registrazione di un nuovo centro vaccinale.
+ * </p>
+ * @author Berti Davide
+ * @author Ivanov Aleksandar Evgeniev
+ * @author Mazza Serghej
+ * @author Rizzi Silvio 719638 VA
+ */
 public class CvRegCentro {
+
+    /**
+     * <p>
+     *     Il costruttore della classe.
+     * </p>
+     * <p>
+     *     <em>NOTA: i dati necessari alla costruzione della GUI sono recuperati nel file <code>.fxml</code> associato alla classe.</em>
+     * </p>
+     */
     public CvRegCentro() {
         try {
             Stage stage = new Stage();

234026a4108d58f74a5bcddf8e3cb0fe907106f1
s93r <84583922+s93r@users.noreply.github.com>
2021-08-15 18:51:21 +0200

    GUIcv : CvHomePage : Javadoc v.1

--- src/main/java/centrivaccinali/CvHomePage.java
+++ src/main/java/centrivaccinali/CvHomePage.java
@@ -6,7 +6,25 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * <p>
+ *     La pagina iniziale dell'applicazione.
+ * </p>
+ * @author Berti Davide
+ * @author Ivanov Aleksandar Evgeniev
+ * @author Mazza Serghej
+ * @author Rizzi Silvio 719638 VA
+ */
 public class CvHomePage {
+
+    /**
+     * <p>
+     *     Il costruttore della classe.
+     * </p>
+     * <p>
+     *     <em>NOTA: i dati necessari alla costruzione della GUI sono recuperati nel file <code>.fxml</code> associato alla classe.</em>
+     * </p>
+     */
     public CvHomePage() {
         try {
             Stage stage = new Stage();

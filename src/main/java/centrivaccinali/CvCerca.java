8c30c5d0f13aab1b36d0aec1bea5b1ac6960772e
s93r <84583922+s93r@users.noreply.github.com>
2021-08-16 18:04:44 +0200

    GUIcv : CvCerca : Javadoc v.1

--- src/main/java/centrivaccinali/CvCerca.java
+++ src/main/java/centrivaccinali/CvCerca.java
@@ -7,20 +7,30 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * GUI del modulo per la selezione di un centro vaccinale registrato.
+ * @author Berti Davide
+ * @author Ivanov Aleksandar Evgeniev
+ * @author Mazza Serghej
+ * @author Rizzi Silvio 719638 VA
+ */
 public class CvCerca {
-        public CvCerca() {
-            try {
-                Stage stage = new Stage();
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/CvCerca.fxml"));
-                Parent root = loader.load();
-                CvCntrlCerca cc = loader.getController();
-                cc.setDati();
-                stage.setTitle("CENTRI VACCINALI - Ricerca Centri Vaccinali");
-                stage.setScene(new Scene(root));
-                stage.show();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
 
+    /**
+     * Costruttore della classe. Utilizza il file <code>CvCerca.fxml</code> per generare correttamente la GUI.
+     */
+    public CvCerca() {
+        try {
+            Stage stage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/CvCerca.fxml"));
+            Parent root = loader.load();
+            CvCntrlCerca cc = loader.getController();
+            cc.setDati();
+            stage.setTitle("CENTRI VACCINALI - Ricerca Centri Vaccinali");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }

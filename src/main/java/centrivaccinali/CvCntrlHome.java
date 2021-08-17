1fcfc874cef5dd16ce901f993ba98914da4a290c
s93r <84583922+s93r@users.noreply.github.com>
2021-08-17 15:40:50 +0200

    GUIcv : CvCntrlHome : Javadoc v.1

--- src/main/java/centrivaccinali/CvCntrlHome.java
+++ src/main/java/centrivaccinali/CvCntrlHome.java
@@ -4,7 +4,23 @@ import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.stage.Stage;
 
+/**
+ * Controller associato a <code>CvHomePage</code>.
+ * @author Berti Davide
+ * @author Ivanov Aleksandar Evgeniev
+ * @author Mazza Serghej
+ * @author Rizzi Silvio 719638 VA
+ * @see CvHomePage
+ */
 public class CvCntrlHome {
+
+    /**
+     * Chiude la finestra corrente e crea una nuova istanza di <code>CvRegCentro</code>. &Egrave; associato al bottone
+     * per la registrazione di un centro vaccinale.
+     * @param event il riferimento all'evento associato
+     * @see CvRegCentro
+     * @see ActionEvent
+     */
     public void clickRegistraCentro(ActionEvent event) {
         // CHIUSURA DELLA VECCHIA FINESTRA
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -12,6 +28,14 @@ public class CvCntrlHome {
         // APERTURA DELLA NUOVA FINESTRA
         new CvRegCentro();
     }
+
+    /**
+     * Chiude la finestra corrente e crea una nuova istanza di <code>CvRegVaccinato</code>. &Egrave; associato al bottone
+     * per la registrazione di un cittadino vaccinato.
+     * @param event il riferimento all'evento associato
+     * @see CvRegVaccinato
+     * @see ActionEvent
+     */
     public void clickRegistraVaccinato(ActionEvent event) {
         // CHIUSURA DELLA VECCHIA FINESTRA
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();

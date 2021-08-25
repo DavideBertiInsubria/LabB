68c01bdd06e2ba6a7019cd4667465ee5b9e6c9fb
s93r <84583922+s93r@users.noreply.github.com>
2021-08-25 15:38:17 +0200

    GUIcv : CvCntrlCerca : Javadoc v.2

--- src/main/java/centrivaccinali/CvCntrlCerca.java
+++ src/main/java/centrivaccinali/CvCntrlCerca.java
@@ -31,12 +31,35 @@ public class CvCntrlCerca {
     @FXML
     private ListView<String> listCentriVaccinali;
 
-    private ArrayList<CentroVaccinale> listaCentriVaccinaliVisualizzati = new ArrayList<CentroVaccinale>();
+    private ArrayList<CentroVaccinale> listaCentriVaccinaliVisualizzati = new ArrayList<>();
 
     private CvCntrlVaccinato CNTRLV;
 
     /**
-     * Invoca il metodo interno <code>compilaLista</code>.
+     * Chiude la finestra corrente e carica il nome del centro vaccinale nel modulo per la registrazione di un cittadino
+     * vaccinato. Questo metodo &egrave; associato al bottone di conferma dell'operazione.
+     *
+     * @param event il riferimento all'evento associato
+     * @see ActionEvent
+     * @see CvCntrlVaccinato
+     */
+    public void clickOK (ActionEvent event){
+        if (listCentriVaccinali.getSelectionModel().getSelectedIndex() == -1 ){
+            JOptionPane.showMessageDialog(null, "Selezionare un centro vaccinale.", "Warning", JOptionPane.WARNING_MESSAGE);
+        } else {
+            try {
+                // CHIUSURA DELLA VECCHIA FINESTRA
+                Stage thisWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                thisWindow.close();
+                CNTRLV.setDati(listaCentriVaccinaliVisualizzati.get(listCentriVaccinali.getSelectionModel().getSelectedIndex()));
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Popola la lista con i nominativi di centri vaccinali registrati nella base di dati dell'applicazione.
      * @throws RemoteException se la connessione al server fallisce
      */
     public void setDati(CvCntrlVaccinato cntrlV) throws RemoteException {
@@ -44,9 +67,6 @@ public class CvCntrlCerca {
         CNTRLV = cntrlV;
     }
 
-    /**
-     * Popola la lista con i nominativi di centri vaccinali registrati in database.
-     */
     private void compilaLista() {
         // COLLEGAMENTO A SERVER
         ServerInterface server = null;
@@ -59,39 +79,16 @@ public class CvCntrlCerca {
         }
         listCentriVaccinali.getItems().clear();
         listaCentriVaccinaliVisualizzati.clear();
-        // SET LISTA
+        // COMPILAZIONE DELLA LISTA
         try {
             listaCentriVaccinaliVisualizzati = server.cercaCentroVaccinale("", "", "");
         } catch (RemoteException e) {
-            JOptionPane.showMessageDialog(null, "Errore con il collegamento al server.");
+            JOptionPane.showMessageDialog(null, "Connessione al server fallita.", "Error", JOptionPane.ERROR_MESSAGE);
             e.printStackTrace();
             System.exit(0);
         }
         for (CentroVaccinale centroVaccinale : listaCentriVaccinaliVisualizzati) {
             listCentriVaccinali.getItems().add(centroVaccinale.getNome() + " - " + centroVaccinale.getIndirizzo());
-        }
-    }
-
-    /**
-     * Chiude la finestra corrente e crea una nuova istanza di <code>CvRegVaccinato</code> in cui &egrave; caricato
-     * il nome del centro vaccinale selezionato. L'applicazione richiede che il nominativo di un centro vaccinale sia
-     * obbligatoriamente selezionato. &Egrave; associato al bottone di conferma.
-     * @param event il riferimento all'evento associato
-     * @see CvRegVaccinato
-     * @see ActionEvent
-     */
-    public void clickOK (ActionEvent event){
-        if (listCentriVaccinali.getSelectionModel().getSelectedIndex() == -1 ){
-            JOptionPane.showMessageDialog(null, "Selezionare un centro vaccinale dalla lista.");
-        } else {
-            try {
-                // CHIUSURA
-                Stage thisWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                thisWindow.close();
-                CNTRLV.setDati(listaCentriVaccinaliVisualizzati.get(listCentriVaccinali.getSelectionModel().getSelectedIndex()));
-            } catch (RemoteException e) {
-                e.printStackTrace();
-            }
         }
     }
 }

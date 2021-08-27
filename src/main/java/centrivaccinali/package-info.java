f6b1bfb56768dfa84cbc909a0cf71e7e0ee4c001
s93r <84583922+s93r@users.noreply.github.com>
2021-08-27 18:32:55 +0200

    GUIcv : package-info file w/ Javadoc

---
+++ src/main/java/centrivaccinali/package-info.java
@@ -0,0 +1,32 @@
+/**
+ * Il package <em>centrivaccinali</em> contiene le classi necessarie alla gestione del client e alla registrazione di
+ * centri vaccinali e cittadini vaccinati. &Egrave; costituito da dieci classi:
+ * <ul>
+ *     <li>CvMain
+ *     <li>CvHomePage
+ *     <li>CvRegCentro
+ *     <li>CvRegVaccinato
+ *     <li>CvCerca
+ *     <li>CvCntrlHome
+ *     <li>CvCntrlCentro
+ *     <li>CvCntrlVaccinato
+ *     <li>CvCntrlCerca
+ *     <li>CvUtil
+ * </ul>
+ *
+ * @author Berti Davide
+ * @author Ivanov Aleksandar Evgeniev
+ * @author Mazza Serghej
+ * @author Rizzi Silvio 719638 VA
+ * @see centrivaccinali.CvMain
+ * @see centrivaccinali.CvHomePage
+ * @see centrivaccinali.CvRegCentro
+ * @see centrivaccinali.CvRegVaccinato
+ * @see centrivaccinali.CvCerca
+ * @see centrivaccinali.CvCntrlHome
+ * @see centrivaccinali.CvCntrlCentro
+ * @see centrivaccinali.CvCntrlVaccinato
+ * @see centrivaccinali.CvCntrlCerca
+ * @see centrivaccinali.CvUtil
+ */
+package centrivaccinali;
